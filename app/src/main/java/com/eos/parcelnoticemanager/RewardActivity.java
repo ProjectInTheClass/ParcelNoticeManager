d88bf6e5922504efffdb3f83b6b8fdf22a5a7ab4
jangbogyeong <jbok6825@naver.com>
2020-09-26 15:20:38 +0900

    Added Notice,Reware acitivity

---
+++ app/src/main/java/com/eos/parcelnoticemanager/RewardActivity.java
@@ -0,0 +1,13 @@
+package com.eos.parcelnoticemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RewardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reward);
+    }
+}
