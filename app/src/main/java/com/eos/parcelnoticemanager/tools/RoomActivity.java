8da7394bd7ffbee07a7c36969e43dd3cc8e06ed9
BraveSejin <ihavebravebiz@gmail.com>
2021-01-05 15:55:53 +0900

    room activity 작동되게 수정

--- app/src/main/java/com/eos/parcelnoticemanager/tools/RoomActivity.java
+++ app/src/main/java/com/eos/parcelnoticemanager/tools/RoomActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -37,7 +38,7 @@ public class RoomActivity extends AppCompatActivity {
     int totalFloor;
     ArrayList<Integer> roomNum = new ArrayList<>();
     private RoomApi roomApi;
-    int i;
+
     static SharedPreferences pref;
 
 
@@ -72,7 +73,7 @@ public class RoomActivity extends AppCompatActivity {
     }
 
     private void init(){
-        totalFloor = 3;
+        totalFloor = 4;
 
         roomApi = new Retrofit.Builder()
                 .baseUrl(getString(R.string.base_url))
@@ -85,20 +86,26 @@ public class RoomActivity extends AppCompatActivity {
             temp.setFloorNum(i + 1);
             globalfloors.add(temp);
         }
-
-        for(i = 0; i<totalFloor; i++){
+        //1 ~ 4층에 대해서 각 층마다 room 정보를 불러오고(cllRoomListByFloor)
+        for(int i = 0; i<totalFloor; i++){
             /*JsonObject jsonObject = new JsonObject();
             jsonObject.addProperty("floor",i+1);*/
-
+            //해당 층에 있는 방들을 불러옵니다. 없을 수도 있어요.
             Call<List<RoomData>> callRoomListByFloor = roomApi.getRooms_byFloor(getToken(), i+1);
+            if (callRoomListByFloor == null)
+                continue;
+            final FloorData floor = globalfloors.get(i);
+            //층에 있는 방들 정보를 불러옵니다.
             Callback<List<RoomData>> callback = new Callback<List<RoomData>>() {
                 @Override
                 public void onResponse(Call<List<RoomData>> call, Response<List<RoomData>> response) {
-                    globalfloors.get(i).setRooms((ArrayList<RoomData>) response.body());
-                    for(int j = 0; j < globalfloors.size(); j++){
-                        System.out.println(globalfloors.get(i).getRooms().get(j));
-                    }
-                    globalfloors.get(i).finalRoomNum = globalfloors.get(i).rooms.size()+globalfloors.get(i).floorNum*100;
+                    Log.d("글로벌 프로어, 토탈 플로어", globalfloors.size()+ ","+totalFloor);
+                    floor.setRooms((ArrayList<RoomData>) response.body());
+//                    for(int j = 0; j < globalfloors.size(); j++){
+//                        System.out.println(globalfloors.get(i).getRooms().get(j));
+//                    }
+                    //방 추가 + 누르면 몇 번째 방까지 있는지 체크.
+//                    floor.get(i).finalRoomNum = globalfloors.get(i).rooms.size()+globalfloors.get(i).floorNum*100;
                 }
                 @Override
                 public void onFailure(Call<List<RoomData>> call, Throwable t) {
