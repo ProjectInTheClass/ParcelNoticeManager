92710e299cdfc0744d9caf376e3e3179b5c52d40
2019088277 <2019088277@hanyang.ac.kr>
2021-01-18 14:07:27 +0900

    review

--- app/src/main/java/com/eos/parcelnoticemanager/tools/RoomActivity.java
+++ app/src/main/java/com/eos/parcelnoticemanager/tools/RoomActivity.java
@@ -12,10 +12,13 @@ import android.widget.Toast;
 
 import com.eos.parcelnoticemanager.R;
 import com.eos.parcelnoticemanager.adapter.FloorAdapter;
+import com.eos.parcelnoticemanager.adapter.ParcelFloorAdapter;
+import com.eos.parcelnoticemanager.data.DormitoryData;
 import com.eos.parcelnoticemanager.data.FloorData;
 import com.eos.parcelnoticemanager.data.RoomData;
 import com.eos.parcelnoticemanager.data.TokenVO;
 import com.eos.parcelnoticemanager.retrofit.AuthApi;
+import com.eos.parcelnoticemanager.retrofit.DormitoryApi;
 import com.eos.parcelnoticemanager.retrofit.RoomApi;
 import com.google.gson.JsonObject;
 
@@ -36,9 +39,9 @@ public class RoomActivity extends AppCompatActivity {
     private FloorAdapter floorAdapter;
     public ArrayList<FloorData> globalfloors = new ArrayList<>();
     int totalFloor;
-    ArrayList<Integer> roomNum = new ArrayList<>();
     private RoomApi roomApi;
-
+    private DormitoryData dormitoryData;
+    private DormitoryApi dormitoryApi;
     static SharedPreferences pref;
 
 
@@ -73,7 +76,6 @@ public class RoomActivity extends AppCompatActivity {
     }
 
     private void init(){
-        totalFloor = 4;
 
         roomApi = new Retrofit.Builder()
                 .baseUrl(getString(R.string.base_url))
@@ -81,25 +83,50 @@ public class RoomActivity extends AppCompatActivity {
                 .build()
                 .create(RoomApi.class);
 
-        for(int i = 0; i<totalFloor; i++) {
-            FloorData temp = new FloorData();
-            temp.setFloorNum(i + 1);
-            globalfloors.add(temp);
-        }
+        dormitoryApi = new Retrofit.Builder()
+                .baseUrl(getString(R.string.base_url))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(DormitoryApi.class);
+
+        dormitoryApi = new Retrofit.Builder()
+                .baseUrl(getString(R.string.base_url))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(DormitoryApi.class);
+
+        Call<DormitoryData> callGetDormitory = dormitoryApi.get_dormitory(getToken());
+        Callback<DormitoryData> callback = new Callback<DormitoryData>() {
+            @Override
+            public void onResponse(Call<DormitoryData> call, Response<DormitoryData> response) {
+                dormitoryData = response.body();
+                totalFloor = dormitoryData.getStory();
+                for(int i=0; i< totalFloor; i++){
+                    FloorData temp = new FloorData();
+                    temp.setFloorNum(i+1);
+                    globalfloors.add(temp);
+                }
+            }
+            @Override
+            public void onFailure(Call<DormitoryData> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),t.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        };
+        callGetDormitory.enqueue(callback);
+
+
         //1 ~ 4층에 대해서 각 층마다 room 정보를 불러오고(cllRoomListByFloor)
         for(int i = 0; i<totalFloor; i++){
-            /*JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("floor",i+1);*/
             //해당 층에 있는 방들을 불러옵니다. 없을 수도 있어요.
             Call<List<RoomData>> callRoomListByFloor = roomApi.getRooms_byFloor(getToken(), i+1);
             if (callRoomListByFloor == null)
                 continue;
             final FloorData floor = globalfloors.get(i);
+
             //층에 있는 방들 정보를 불러옵니다.
-            Callback<List<RoomData>> callback = new Callback<List<RoomData>>() {
+            Callback<List<RoomData>> callback2 = new Callback<List<RoomData>>() {
                 @Override
                 public void onResponse(Call<List<RoomData>> call, Response<List<RoomData>> response) {
-                    Log.d("글로벌 프로어, 토탈 플로어", globalfloors.size()+ ","+totalFloor);
                     floor.setRooms((ArrayList<RoomData>) response.body());
 //                    for(int j = 0; j < globalfloors.size(); j++){
 //                        System.out.println(globalfloors.get(i).getRooms().get(j));
@@ -110,10 +137,9 @@ public class RoomActivity extends AppCompatActivity {
                 @Override
                 public void onFailure(Call<List<RoomData>> call, Throwable t) {
                     Toast.makeText(RoomActivity.this,t.getMessage(),Toast.LENGTH_LONG).show();
-
                 }
             };
-            callRoomListByFloor.enqueue(callback);
+            callRoomListByFloor.enqueue(callback2);
         }
     }
     public static String getToken(){
