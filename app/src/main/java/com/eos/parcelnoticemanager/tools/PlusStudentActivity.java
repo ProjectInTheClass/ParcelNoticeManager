217844a898b3b7c8c2b8f4e8d02b0180b19c3deb
2019088277 <2019088277@hanyang.ac.kr>
2021-01-21 13:54:08 +0900

    Toast가 안떠...

--- app/src/main/java/com/eos/parcelnoticemanager/tools/PlusStudentActivity.java
+++ app/src/main/java/com/eos/parcelnoticemanager/tools/PlusStudentActivity.java
@@ -26,6 +26,7 @@ import com.eos.parcelnoticemanager.R;
 import com.eos.parcelnoticemanager.adapter.RoomAdapter;
 import com.eos.parcelnoticemanager.data.DormitoryData;
 import com.eos.parcelnoticemanager.data.FloorData;
+import com.eos.parcelnoticemanager.data.ResponseData;
 import com.eos.parcelnoticemanager.data.StudnetInRoomData;
 import com.eos.parcelnoticemanager.data.UserData;
 import com.eos.parcelnoticemanager.retrofit.RoomApi;
@@ -46,6 +47,7 @@ import static com.eos.parcelnoticemanager.tools.RoomActivity.getToken;
 public class PlusStudentActivity extends AppCompatActivity {
     private ArrayList<String> list; //자동완성될 데이터
     private SagamApi sagamApi;
+    private RoomApi roomApi;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,11 +65,17 @@ public class PlusStudentActivity extends AppCompatActivity {
                 .build()
                 .create(SagamApi.class);
 
+        roomApi = new Retrofit.Builder()
+                .baseUrl(getString(R.string.base_url))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(RoomApi.class);
+
         Call<List<UserData>> callGetUser = sagamApi.getUsers(getToken());
         Callback<List<UserData>> callback = new Callback<List<UserData>>() {
             @Override
             public void onResponse(Call<List<UserData>> call, Response<List<UserData>> response) {
-                ArrayList<UserData> userList = new ArrayList<>(response.body());
+                final ArrayList<UserData> userList = new ArrayList<>(response.body());
 
                 for (int i = 0; i < userList.size(); i++) {
                     list.add(userList.get(i).getName() + " (" + userList.get(i).getPhoneNum() + ")");
@@ -76,7 +84,50 @@ public class PlusStudentActivity extends AppCompatActivity {
                 autoCompleteTextView.setAdapter(new ArrayAdapter<String>(context,
                         android.R.layout.simple_dropdown_item_1line, list));
 
-                Log.d("roomId", Integer.toString(RoomAdapter.whichId));
+
+                Button btn_yes = (Button)findViewById(R.id.button_register_yes);
+                Button btn_no = (Button)findViewById(R.id.button_register_no);
+
+                btn_yes.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        String who = autoCompleteTextView.getText().toString();
+                        int index;
+                        for(index = 0; index < list.size(); index++){
+                            if(list.get(0).equals(who)) break;
+                        }
+                        int id = userList.get(index).getId();
+                        JsonObject json = new JsonObject();
+                        json.addProperty("roomId", RoomAdapter.whichId);
+                        json.addProperty("userId", id);
+
+                        //호실 update 정보를 서버에 반영합니다.
+                        Call<ResponseData> callUpdateRoom = roomApi.update_user(getToken(),json);
+                        Callback<ResponseData> callback = new Callback<ResponseData>() {
+                            @Override
+                            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                                Toast.makeText(context,"등록이 완료되었습니다.",Toast.LENGTH_LONG).show();
+                            }
+
+                            @Override
+                            public void onFailure(Call<ResponseData> call, Throwable t) {
+                                Toast.makeText(context,t.getMessage(),Toast.LENGTH_LONG).show();
+                            }
+                        };
+
+                    }
+                });
+                btn_no.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(context, RoomActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+
+
+
 
             }
 
